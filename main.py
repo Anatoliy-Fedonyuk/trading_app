49008d50efb13cdb272fdc015177fefe284344c0
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-09-11 13:05:04 +0200

    ver 0.03

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI
+from pydantic import BaseModel, Field
 from uvicorn import run
+from datetime import datetime
+from enum import Enum
+
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ResponseValidationError
+from fastapi.responses import JSONResponse
 
 app = FastAPI(
     title="Trading App"
 )
+
+
+# Благодаря этой функции клиент видит ошибки, происходящие на сервере, вместо "Internal server error"
+@app.exception_handler(ResponseValidationError)
+async def validation_exception_handler(request: Request, exc: ResponseValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors()}),
+    )
+
 
 fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
@@ -23,23 +40,19 @@ fake_trades = [
 ]
 
 
-@app.get("/trades")
-def get_trades(limit: int = 2, offset: int = 0):
-    return fake_trades[offset:][:limit]
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
 
 
-fake_users2 = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-]
-
-
-@app.put("/users/{user_id}")
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
-    current_user["name"] = new_name
-    return {"status": 200, "data": current_user}
+@app.post("/trades")
+def get_trades(trades: list[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
 
 
 if __name__ == "__main__":
