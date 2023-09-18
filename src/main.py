9e2f8f9b89f0f0b7529bc0665bb6b2ff93bd5860
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-09-18 22:03:27 +0200

    ver 0.7.4

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
 
 from src.auth.base_config import auth_backend, fastapi_users
 from src.auth.schemas import UserRead, UserCreate
@@ -24,8 +27,13 @@ app.include_router(
 app.include_router(router_operation)
 
 
+@app.on_event("startup")
+async def startup():
+    redis = aioredis.from_url("redis://localhost")
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+
 if __name__ == "__main__":
     from uvicorn import run
 
     run("src.main:app", reload=True)
-
--- src/operations/router.py
+++ src/operations/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi_cache.decorator import cache
+import time
 
 from src.database import get_async_session
 from src.operations.models import operation
@@ -12,7 +14,15 @@ router = APIRouter(
 )
 
 
+@router.get("/long_operation")
+@cache(expire=60)
+def get_long_op():
+    time.sleep(2)
+    return "Много много данных, которые вычислялись сто лет"
+
+
 @router.get("")
+@cache(expire=60)
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
     try:
         query = select(operation).filter(operation.c.type == operation_type)
