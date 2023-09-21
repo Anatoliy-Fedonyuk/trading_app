57d5ef845bfcc25c634a47ccf82968f178879d95
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-09-21 17:07:42 +0200

    ver 0.7.4

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest, time
 from httpx import AsyncClient
 
 from tests.conftest import cache_initialized
@@ -19,19 +19,29 @@ async def test_add_specific_operations(ac: AsyncClient):
 
 @pytest.mark.asyncio
 async def test_get_specific_operations(ac: AsyncClient, cache_initialized):
-    response = await ac.get("/operations", params={
-        "operation_type": "Выплата купонов",
-    })
+    # Замеряем время выполнения первого запроса
+    start_time = time.time()
+    response = await ac.get("/operations", params={"operation_type": "Выплата купонов"})
+    end_time = time.time()
+    first_request_duration = end_time - start_time
+
     assert response.status_code == 200
     assert response.json()["status"] == "success"
     assert len(response.json()["data"]) == 1
 
-    response_cached = await ac.get("/operations", params={
-        "operation_type": "Выплата купонов", })
+    # Замеряем время выполнения второго запроса
+    start_time = time.time()
+    response_cached = await ac.get("/operations", params={"operation_type": "Выплата купонов"})
+    end_time = time.time()
+    second_request_duration = end_time - start_time
 
     assert response_cached.status_code == 200
     assert response_cached.json()["status"] == "success"
     assert len(response_cached.json()["data"]) == 1
+
+    # Сравниваем время выполнения
+    print(f'{second_request_duration=} < {first_request_duration=}')
+    assert second_request_duration < first_request_duration
 
 
 if __name__ == '__main__':
