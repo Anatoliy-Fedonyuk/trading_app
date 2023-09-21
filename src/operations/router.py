70b29a9760bada9645fe8ee327a4108b2b865554
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-09-21 12:34:47 +0200

    ver 0.7.4

--- src/operations/router.py
+++ src/operations/router.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi_cache.decorator import cache
 import time
 
-from src.auth.base_config import current_user
 from src.database import get_async_session
 from src.operations.models import operation
 from src.operations.schemas import OperationCreate
@@ -23,35 +22,35 @@ def get_long_op():
 
 
 @router.get("")
-# @cache(expire=60)
+@cache(expire=60)
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
     try:
         query = select(operation).filter(operation.c.type == operation_type)
         result = await session.execute(query)
-        rows = result.fetchall()
-        columns = result.keys()
-        records = [dict(zip(columns, row)) for row in rows]
-        return {
-            "status": "success",
-            "data": records,
-            "details": None
-        }
-    except Exception:
-        # Передать ошибку разработчикам - Логировать!
+        return {"status": "success",
+                "data": result.mappings().all(),
+                "details": None}
+    except Exception as e:
+        # Логируйте ошибку и возвращайте более информативное сообщение.
         raise HTTPException(status_code=500, detail={
             "status": "error",
             "data": None,
-            "details": None
-        })
+            "details": str(e)})
 
 
 @router.post("")
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
-    stmt = insert(operation).values(**new_operation.dict())
-    await session.execute(stmt)
-    await session.commit()
-    return {"status": "success"}
-
+    try:
+        stmt = insert(operation).values(**new_operation.dict())
+        await session.execute(stmt)
+        await session.commit()
+        return {"status": "Add operation success"}
+    except Exception as ex:
+        # Логируйте ошибку и возвращайте более информативное сообщение.
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": str(ex)})
 
 # @router.get("/main") # Глупость какая-то
 # async def main(session: AsyncSession = Depends(get_async_session)):
