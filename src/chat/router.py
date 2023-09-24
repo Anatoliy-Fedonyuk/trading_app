f656fe85e71759dae52aa55cea951674bea46873
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-09-24 23:27:06 +0200

    ver 0.7.4

--- src/chat/router.py
+++ src/chat/router.py
@@ -4,7 +4,7 @@ from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.chat.models import Messages
-# from src.chat.schemas import MessagesModel
+from src.chat.schemas import MessagesModel
 from src.database import async_session_maker, get_async_session
 
 router = APIRouter(prefix="/chat",
@@ -44,13 +44,22 @@ class ConnectionManager:
 manager = ConnectionManager()
 
 
+@router.get("/last_messages")
+async def get_last_messages(
+        session: AsyncSession = Depends(get_async_session),
+) -> list[MessagesModel]:
+    query = select(Messages).order_by(Messages.id.desc()).limit(5)
+    messages = await session.execute(query)
+    return messages.scalars().all()
+
+
 @router.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(f"Client #{client_id} says: {data}", add_to_db=True)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        await manager.broadcast(f"Client #{client_id} left the chat", add_to_db=False)
