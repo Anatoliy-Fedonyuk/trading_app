3e39673ebb07bfd1ef2ff800c2c24552f13dbd55
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-09-26 19:47:41 +0200

    ver 0.7.4

--- src/chat/models.py
+++ src/chat/models.py
@@ -9,5 +9,6 @@ class Messages(Base):
     id = Column(Integer, primary_key=True)
     messages = Column(String(length=1024), nullable=False)
 
-    # def as_dict(self):
-    #     return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
