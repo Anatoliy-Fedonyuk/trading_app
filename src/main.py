65b6f710cf3bbc197b376f78bdfe447ca57063c1
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-09-27 18:49:48 +0200

    ver 0.7.4

--- src/main.py
+++ src/main.py
@@ -35,16 +35,16 @@ app.include_router(router_tasks)
 app.include_router(router_pages)
 app.include_router(router_chat)
 
-# origins = ['http://localhost:3000', ]
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
-#     allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-#                    "Authorization"],
-# )
+origins = ['http://localhost:3000', ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
 
 @app.on_event("startup")
